124598c32a35136646c65ad616b8719d9cac84d3
Victor Samoilov <victor.samoilov.1234@gmail.com>
2022-12-18 15:59:48 +0300

    Add pipelines

--- README.md
+++ README.md
@@ -10,9 +10,51 @@ python3 decision_tree/main.py
 * Форматеры: `black` и `isort`
 * Линтеры: `flake8`
 * Плагины:
-  * `flake8-quotes` 
-  * `flake8-comprehensions` 
-  * `flake8-return` 
+  * `flake8-quotes`
+  * `flake8-comprehensions`
+  * `flake8-return`
   * `flake8-literal`
   * `flake8-variables-names`
 
+# HW 5
+
+Использую DVC.
+
+## DAG
+```bash
+              +--------------+
+              | prepare_data |
+              +--------------+
+           ***        *      ****
+        ***           *          ***
+      **              *             ***
++-----+               *                **
+| fit |*              *                 *
++-----+ ***           *                 *
+    *      ***        *                 *
+    *         ***     *                 *
+    *            **   *                 *
+    **          +---------+            **
+      ***       | predict |         ***
+         ***    +---------+      ***
+            **        *      ****
+              ***     *   ***
+                 **   * **
+                 +-------+
+                 | plots |
+                 +-------+
++--------------------+
+| plots/tree.png.dvc |
++--------------------+
++-------------------------+
+| plots/roc_curve.png.dvc |
++-------------------------+
++------------------+
+| plots/2d.png.dvc |
++------------------+
+```
+
+С помощью команды `dvc dag`
+
+## CLI и запуск пайплайна
+`dvc repro`
--- decision_tree/main.py
+++ decision_tree/main.py
@@ -1,4 +1,6 @@
+import argparse
 import os.path
+import pickle
 
 from sklearn.datasets import make_moons
 
@@ -7,19 +9,66 @@ from decision_tree.draw_tree import draw_tree
 from decision_tree.plots import plot_2d, plot_roc_curve
 
 
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--prepare_data", action="store_true")
+    parser.add_argument("--fit", action="store_true")
+    parser.add_argument("--predict", action="store_true")
+    parser.add_argument("--plots", action="store_true")
+    return parser.parse_args()
+
+
+def dump(obj, file_name):
+    with open(file_name, "wb") as f:
+        pickle.dump(obj, f)
+
+
+def load(file_name):
+    with open(file_name, "rb") as f:
+        return pickle.load(f)
+
+
 def main():
-    noise = 0.35
-    X, y = make_moons(1500, noise=noise)
-    X_test, y_test = make_moons(200, noise=noise)
-    tree = DecisionTreeClassifier(max_depth=5, min_samples_leaf=30)
-    tree.fit(X, y)
-    if not os.path.exists("../plots"):
-        os.mkdir("../plots")
-    plot_2d(tree, X, y, save_path="../plots/2d.png")
-    plot_roc_curve(
-        y_test, tree.predict_proba(X_test), save_path="../plots/roc_curve.png"
-    )
-    draw_tree(tree, save_path="../plots/tree.png")
+    args = get_args()
+    if args.prepare_data:
+        noise = 0.35
+        X_train, y_train = make_moons(1500, noise=noise)
+        X_test, y_test = make_moons(200, noise=noise)
+
+        if not os.path.exists("data"):
+            os.mkdir("data")
+        dump(X_train, "data/train_x")
+        dump(y_train, "data/train_y")
+        dump(X_test, "data/test_x")
+        dump(y_test, "data/test_y")
+    elif args.fit:
+        tree = DecisionTreeClassifier(max_depth=5, min_samples_leaf=30)
+        X_train = load("data/train_x")
+        y_train = load("data/train_y")
+        tree.fit(X_train, y_train)
+
+        if not os.path.exists("model"):
+            os.mkdir("model")
+        dump(tree, "model/tree")
+    elif args.predict:
+        tree = load("model/tree")
+        X_test = load("data/test_x")
+        pred_y = tree.predict_proba(X_test)
+        dump(pred_y, "data/pred_y")
+    elif args.plots:
+        X_train = load("data/train_x")
+        y_train = load("data/train_y")
+        tree = load("model/tree")
+        y_test = load("data/test_y")
+        y_pred = load("data/pred_y")
+
+        if not os.path.exists("plots"):
+            os.mkdir("plots")
+        plot_2d(tree, X_train, y_train, save_path="plots/2d.png")
+        plot_roc_curve(y_test, y_pred, save_path="plots/roc_curve.png")
+        draw_tree(tree, save_path="plots/tree.png")
+    else:
+        print(os.getcwd())
 
 
 if __name__ == "__main__":
